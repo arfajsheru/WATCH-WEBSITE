1317c5972f8836af334845a59ecc5427843230bf
arfajsheru <arfatsheru74@gmail.com>
2025-01-03 16:34:58 +0530

    watch website sidebar big update

--- Watch-Project/src/main.jsx
+++ Watch-Project/src/main.jsx
@@ -3,11 +3,11 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
 import "./index.css";
 import App from "./App.jsx";
-import ShopContextProvider from "./Context/ContextShop.jsx";
+import shopContextProvider from "./Context/ContextShop.jsx";
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-  <ShopContextProvider>
+  <shopContextProvider>
     <App />
-  </ShopContextProvider>
+  </shopContextProvider>
   </BrowserRouter>
 );

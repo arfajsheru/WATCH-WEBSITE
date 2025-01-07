c0698ca8120fba191563509b19b94e45080daa56
arfajsheru <arfatsheru74@gmail.com>
2025-01-07 19:38:31 +0530

    update banner bar

--- Watch-Project/src/Component/BestSellers.jsx
+++ Watch-Project/src/Component/BestSellers.jsx
@@ -12,11 +12,12 @@ const BestSellers = () => {
   );
 
   return (
-    <div className="p-8">
+    <div className="p-8 mx-5 sm:mx-6 md:mx-2">
       <div className="mb-2 md:mb-3">
-        <h1 className="cursor-pointer text-base font-semibold text-center md:font-bold uppercase md:text-4xl text-gray-700">
-          Browse Our BestSellers
-        </h1>
+      <h1 className="cursor-pointer text-xl md:text-5xl font-medium text-center text-gray-600 tracking-wide uppercase transition-all duration-300 hover:text-secondary">
+  Browse Our BestSellers
+</h1>
+
       </div>
 
       <div className="mb-5 flex items-center gap-2 md:gap-3 font-bold font-font2 text-gray-700">
---
+++ Watch-Project/src/Component/Title.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Title = ({title1, title2}) => {
+  return (
+    <div class="seven">
+      <h1>Style Seven</h1>
+    </div>
+  );
+};
+
+export default Title;
